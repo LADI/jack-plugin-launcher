d0a09907f54dffb4b7efd8a5982d98b05d293ba7
Nedko Arnaudov <nedko@nedk.org>
2022-08-29 18:58:59 +0300

    jpl: adjust the welcome message

--- main.c
+++ main.c
@@ -14,13 +14,13 @@
 
 int main(void)
 {
-  printf("JACK Plugin Launcher, version " JPL_VERSION
+  printf(
+    "JACK Plugin Launcher, version " JPL_VERSION "\n"
 #if (HAVE_GITVERSION_H)
-         " (" GIT_VERSION ")"
+    "(" GIT_VERSION ")\n"
 #endif
-         " built on " BUILD_TIMESTAMP
-         "\n"
+    "built on " BUILD_TIMESTAMP"\n"
+    "Copyleft JACK Plugin Authors\n"
     );
-  printf("Copyleft 2022 Nedko Arnaudov\n");
   return 0;
 }
